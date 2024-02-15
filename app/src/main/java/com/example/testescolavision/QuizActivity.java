86c1cb42f43587c00861c6b3149c985c2f7f78a6
adrianrs2510 <adrianrs2510@gmail.com>
2024-02-15 18:05:49 +0100

    c2

--- app/src/main/java/com/example/testescolavision/QuizActivity.java
+++ app/src/main/java/com/example/testescolavision/QuizActivity.java
@@ -125,5 +125,15 @@ public class QuizActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.botonNext);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
+            }
+        });
     }
 }
