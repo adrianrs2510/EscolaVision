0a80dda33d9c0e3f3a05a05252f283b34e57902f
adrianrs2510 <adrianrs2510@gmail.com>
2024-02-20 10:03:21 +0100

    FUNCIONA SE VEEEEEEEEEEEEE

--- app/src/main/java/com/example/testescolavision/MainActivity.java
+++ app/src/main/java/com/example/testescolavision/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.testescolavision;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.*;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -10,7 +13,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements EscolaVisionActivityInterface{
+public class MainActivity extends AppCompatActivity {
     private Button mBoton1;
     private Button mBoton2;
     private Button mBoton3;
@@ -90,7 +93,7 @@ public class MainActivity extends AppCompatActivity implements EscolaVisionActiv
     private static final String TAG = "MainActivity";
     private static final String KEY_INDEX = "index";
     @Override
-    public void onCreat(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_escola_vision);
@@ -199,7 +202,7 @@ public class MainActivity extends AppCompatActivity implements EscolaVisionActiv
         super.onResume();
         Log.d(TAG, "onResume() called");
     }
-    @Override
+
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu);
         inflater.inflate(R.menu.fragment_main_list, menu);
@@ -210,7 +213,7 @@ public class MainActivity extends AppCompatActivity implements EscolaVisionActiv
         Log.d(TAG, "onPause() called");
     }
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         Log.i(TAG, "onSaveInstanceState");
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
