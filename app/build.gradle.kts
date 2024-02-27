2c0f3ef3ddba5af40048d7f7b6c8a1671ed0c0db
adrianrs2510 <adrianrs2510@gmail.com>
2024-02-27 13:56:23 +0100

    app terminada para publicar Version 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.escolavision.testescolavision"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "3"
+        versionCode = 5
+        versionName = "5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
