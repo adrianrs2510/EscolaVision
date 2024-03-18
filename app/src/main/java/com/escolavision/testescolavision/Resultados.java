a4d8108cd765b4687eff27ae2ed55294b3f52b2f
adrianrs2510 <adrianrs2510@gmail.com>
2024-03-18 16:39:35 +0100

    Revert "Cambios finales Resultados y version para publicar"
    This reverts commit 65dc364fecbc25d39a1b308beed66a33582dc0ca.

--- app/src/main/java/com/escolavision/testescolavision/Resultados.java
+++ app/src/main/java/com/escolavision/testescolavision/Resultados.java
@@ -2,6 +2,7 @@ package com.escolavision.testescolavision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
