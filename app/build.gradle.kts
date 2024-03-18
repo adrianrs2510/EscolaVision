7d16c2fc9594cab745e8eae0a5c596389d785ade
adrianrs2510 <adrianrs2510@gmail.com>
2024-03-18 16:49:49 +0100

    actuliazzacion icono y version para publicar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.escolavision.testescolavision"
         minSdk = 24
         targetSdk = 34
-        versionCode = 15
-        versionName = "15.0"
+        versionCode = 16
+        versionName = "16.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
